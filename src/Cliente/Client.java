7abd946256e1fe14b793d66fd8df5affb50d950d
Cristian <cristianmatias52@gmail.com>
2019-12-13 15:33:28 +0100

    Los memes son reales si crees en ellos

--- src/Cliente/Client.java
+++ src/Cliente/Client.java
@@ -215,11 +215,13 @@ public class Client extends JFrame {
         //======== dialogPane ========
         {
             dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
-            dialogPane.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder(
-            0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder
-            . BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color.
-            red) ,dialogPane. getBorder( )) ); dialogPane. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .
-            beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+            dialogPane.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new
+            javax.swing.border.EmptyBorder(0,0,0,0), "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e",javax
+            .swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java
+            .awt.Font("Dialo\u0067",java.awt.Font.BOLD,12),java.awt
+            .Color.red),dialogPane. getBorder()));dialogPane. addPropertyChangeListener(new java.beans.
+            PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("borde\u0072".
+            equals(e.getPropertyName()))throw new RuntimeException();}});
             dialogPane.setLayout(new BorderLayout());
 
             //======== contentPanel ========
@@ -237,7 +239,7 @@ public class Client extends JFrame {
                 contentPanel.add(textField);
                 textField.setBounds(40, 300, 335, 30);
                 contentPanel.add(users);
-                users.setBounds(170, 40, 190, users.getPreferredSize().height);
+                users.setBounds(200, 40, 190, users.getPreferredSize().height);
 
                 //======== scrollPane1 ========
                 {
@@ -252,7 +254,7 @@ public class Client extends JFrame {
                 //---- label1 ----
                 label1.setText("Usuarios: ");
                 contentPanel.add(label1);
-                label1.setBounds(105, 40, 70, 25);
+                label1.setBounds(125, 45, 70, 25);
 
                 //---- label2 ----
                 label2.setText("Mensajes recibidos");
